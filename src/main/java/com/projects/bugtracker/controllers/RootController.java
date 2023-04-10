b24d6f85caa76ab69a4f0ec279601f66b82b0fda
skorotky <alexander.korotkyi@gmail.com>
2023-04-09 20:00:33 -0400

    connected client to server

--- src/main/java/com/projects/bugtracker/controllers/RootController.java
+++ src/main/java/com/projects/bugtracker/controllers/RootController.java
@@ -2,12 +2,11 @@ package com.projects.bugtracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
     @GetMapping({"", "/"})
     public String root(){
-        return "index";
+        return "index.html";
     }
 }
