5214beb715eae630d3be0a3210257f0e5b52cb7f
skorotky <alexander.korotkyi@gmail.com>
2023-05-25 14:09:33 -0400

    added method and query security for repositories

--- src/main/java/com/bugtracker/api/repositories/ProjectRepository.java
+++ src/main/java/com/bugtracker/api/repositories/ProjectRepository.java
@@ -1,13 +1,27 @@
 package com.bugtracker.api.repositories;
 
 import com.bugtracker.api.entities.Project;
+import com.bugtracker.api.entities.User;
+import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    Page<Project> findByOwner_Username(String username, Pageable pageable);
+    @Query("SELECT p FROM Project p WHERE owner=:user " +
+            "AND (p.owner=?#{ principal?.user } OR ?#{ principal?.user } MEMBER of p.collaborators " +
+            "OR p.isPrivate=false OR 1=?#{hasRole('ADMIN')? 1 : 0})")
+    Page<Project> findByOwner(User user, Pageable pageable);
 
-    Page<Project> findByCollaborators_Username(String username, Pageable pageable);
+    @Query("SELECT p FROM Project p WHERE :user MEMBER of p.collaborators " +
+            "AND (p.owner=?#{ principal?.user } OR ?#{ principal?.user } MEMBER of p.collaborators " +
+            "OR p.isPrivate=false OR 1=?#{hasRole('ADMIN')? 1 : 0})")
+    Page<Project> findByCollaborator(User user, Pageable pageable);
+
+    @Query("SELECT p FROM Project p WHERE " +
+            "p.owner=?#{ principal?.user } OR ?#{ principal?.user } MEMBER of p.collaborators " +
+            "OR p.isPrivate=false OR 1=?#{hasRole('ADMIN')? 1 : 0}")
+    @NonNull Page<Project> findAll(@NonNull Pageable pageable);
 }
